33d51ced2615cd7821a73e030f7d2db899031593
mohammed essam <mohammedessameldincs@gmail.com>
2026-06-20 17:06:23 +0300

    Problem B solved

---
+++ Intro To Graph & DFS/ProblemB.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> adj;
+vector<bool> vis;
+
+void dfs(int u)
+{
+    vis[u] = true;
+
+    for (int v : adj[u])
+    {
+        if (!vis[u])
+        {
+            dfs(u);
+        }
+    }
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+
+    if (m != n - 1)
+    {
+        cout << "NO\n";
+        return 0;
+    }
+
+    adj.resize(n + 1);
+    vis.assign(n + 1, false);
+
+    for (int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    dfs(1);
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (!vis[i])
+        {
+            cout << "NO\n";
+            return 0;
+        }
+    }
+
+    cout << "YES\n";
+}
