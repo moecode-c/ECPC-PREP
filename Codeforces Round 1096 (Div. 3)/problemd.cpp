58a2d7c2da825bbb40bcdca9b65193f8925fb03c
mohammed essam <mohammedessameldincs@gmail.com>
2026-07-03 03:47:11 +0300

    Refactor problem-solving logic and enhance input handling in main function

--- Codeforces Round 1096 (Div. 3)/problemd.cpp
+++ Codeforces Round 1096 (Div. 3)/problemd.cpp
@@ -3,22 +3,58 @@ using namespace std;
 
 using ll = long long;
 
-void fast_io() {
+void fast_io()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 }
 
-void solve() {
-    // Write solution here
+int n;
+vector<int> v;
+
+int solve(int l, int r)
+{
+    set<int> st;
+
+    for (int i = 0; i <= n; i++)
+        st.insert(i);
+
+    while (l >= 0 && r < n * 2 && v[l] == v[r])
+    {
+        st.erase(v[l]);
+        l--, r++;
+    }
+
+    return *st.begin();
 }
 
-int main() {
+int main()
+{
     fast_io();
 
     int t;
     cin >> t;
-    while (t--) {
-        solve();
+
+    while (t--)
+    {
+        cin >> n;
+        v.assign(n * 2, 0);
+        for (int i = 0; i < n * 2; i++)
+            cin >> v[i];
+
+        int x = -1, y;
+        for (int i = 0; i < 2 * n; i++)
+        {
+            if (!v[i])
+            {
+                if (~x)
+                    y = i;
+                else
+                    x = i;
+            }
+        }
+
+        cout << max({solve(x, x), solve(y, y), solve((x + y) / 2, (x + y + 1) / 2)}) << endl;
     }
 
     return 0;
