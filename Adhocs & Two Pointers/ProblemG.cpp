4518738d6103f81837e2953432e6a6d8feb79532
mohammed essam <mohammedessameldincs@gmail.com>
2026-08-15 12:17:58 +0300

    Add implementation for Problem G: calculate maximum sum of flowers within constraints

--- Adhocs & Two Pointers/ProblemG.cpp
+++ Adhocs & Two Pointers/ProblemG.cpp
@@ -3,22 +3,50 @@ using namespace std;
 
 using ll = long long;
 
-void fast_io() {
+void fast_io()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 }
 
-void solve() {
-    // Write solution here
+ll m, n;
+vector<ll> flowers;
+
+ll solve()
+{
+    ll sum = 0, l = 0 , r = 0;
+    ll ans = 0;
+
+    while(r < n){
+        sum += flowers[r];
+
+        while(l <= r && (sum > m || flowers[r] - flowers[l] > 1)){
+            sum -= flowers[l];
+            l++;
+        }
+
+        ans = max(ans , sum);
+        r++;
+    }
+
+    return ans;
 }
 
-int main() {
+int main()
+{
     fast_io();
 
     int t;
     cin >> t;
-    while (t--) {
-        solve();
+    while (t--)
+    {
+        cin >> n >> m;
+        flowers.assign(n, 0);
+        for(int i = 0 ; i < n ; i++)
+        cin >> flowers[i];
+        sort(flowers.begin(),flowers.end());
+
+        cout << solve() << endl;
     }
 
     return 0;
