392bb0f19c6bf11a7b211c7420e62f37ddb33427
mohammed essam <mohammedessameldincs@gmail.com>
2026-08-16 23:30:12 +0300

    Add initial implementation of Problem B and Problem C: binary search for element index in a sorted array

---
+++ Binary Search/problemB.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+void fast_io()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+int main()
+{
+    fast_io();
+
+    int n, q;
+    cin >> n >> q;
+
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+
+    int x;
+    while (q--)
+    {
+        cin >> x;
+        int l = 0, r = n - 1;
+        int ans = -1;
+        while (l <= r)
+        {
+            int mid = l + (r - l) / 2;
+
+            if(v[mid] >= x){
+                ans = mid;
+                r = mid - 1;
+            }
+            else{
+                l = mid + 1;
+            }
+        }
+
+        cout << (ans == -1 ? -1 : ans + 1) << endl;
+    }
+
+    return 0;
+}
