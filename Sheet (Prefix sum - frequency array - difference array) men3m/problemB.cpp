01aeaaf1fb8e315f5738c55c6f5f304154febbf4
mohammed essam <mohammedessameldincs@gmail.com>
2026-08-25 06:02:33 +0300

    Add initial implementation of Problem B solution

---
+++ Sheet (Prefix sum - frequency array - difference array) men3m/problemB.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+ 
+using ll = long long; 
+#define int long long 
+#define double long double 
+#define all(x) (x).begin(), (x).end() 
+ 
+void fast_io() { 
+    ios::sync_with_stdio(false); 
+    cin.tie(nullptr); 
+} 
+ 
+int main() { 
+    fast_io(); 
+ 
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> a(n + 1);
+    vector<int> ans(n + 1);
+    vector<bool> visited(100001, false);
+
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+    }
+
+    int distinct = 0;
+
+    for (int i = n; i >= 1; i--) {
+        if (!visited[a[i]]) {
+            visited[a[i]] = true;
+            distinct++;
+        }
+
+        ans[i] = distinct;
+    }
+
+    while (m--) {
+        int l;
+        cin >> l;
+        cout << ans[l] << '\n';
+    }
+
+    return 0; 
+}
