aa118e02542673fc7823c20ffce0947dc00b1821
mohammed essam <mohammedessameldincs@gmail.com>
2026-09-02 00:13:51 +0300

    Add initial implementation of Problem H solution

---
+++ Number Theory Sheet mn3m/problemH.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+#define int long long
+#define double long double
+#define all(x) (x).begin(), (x).end()
+
+void fast_io() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+int32_t main() {
+    fast_io();
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int x;
+        cin >> x;
+
+        cout << 1 << " " << x - 1 << endl;
+    }
+
+    return 0;
+}
