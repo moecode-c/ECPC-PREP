5628e63cb51b5505984f3c8d7d1a5f0dde9119a8
mohammed essam <mohammedessameldincs@gmail.com>
2026-09-04 03:07:55 +0300

    Refactor Problem A solution to use a sieve for prime factorization and improve efficiency

--- Number Theory Sheet mn3m/problemA.cpp
+++ Number Theory Sheet mn3m/problemA.cpp
@@ -6,31 +6,43 @@ using ll = long long;
 #define double long double
 #define all(x) (x).begin(), (x).end()
 
+const int MAXV = 1000001;
+vector<int> spf(MAXV);
+
+void sieve() {
+    for (int i = 2; i < MAXV; i++) {
+        if (spf[i] == 0) {
+            for (int j = i; j < MAXV; j += i)
+                if (spf[j] == 0)
+                    spf[j] = i;
+        }
+    }
+}
+
 void fast_io() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 }
 
-
 int32_t main() {
     fast_io();
+    sieve();
 
     int t;
     cin >> t;
     while (t--) {
         int x;
         cin >> x;
-        int count = 0;
-        for(int i = 1 ; i * i <= x ;i++){
-            if(x % i == 0){
-            count++;
 
-            if(i * i != x)
-            count++;
-            }
+        int count = 1;
+        while (x > 1) {
+            int p = spf[x];
+            int cnt = 0;
+            while (x % p == 0) { x /= p; cnt++; }
+            count *= (cnt + 1);
         }
 
-        cout << count << endl;
+        cout << count << "\n";
     }
 
     return 0;
