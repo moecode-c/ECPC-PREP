ae642b451328fd1ae35bca6c1893c62f45dd834d
mohammed essam <mohammedessameldincs@gmail.com>
2026-09-13 13:14:20 +0300

    Add initial implementation for Problem C solution to count set bits in an integer

---
+++ MN3M/Bitmasks Sheet mn3m/problemC.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+#define int long long
+#define double long double
+#define all(x) (x).begin(), (x).end()
+
+void fast_io() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+int32_t main() {
+    fast_io();
+
+    int n;
+    cin >> n;
+
+    cout << __builtin_popcount(n);
+
+    return 0;
+}
