b17a5e58e3c9ae614cbbf2d6c617d8f66ab92add
mohammed essam <mohammedessameldincs@gmail.com>
2026-09-21 22:35:05 +0300

    Add initial implementation for Problem M solution to calculate cost based on given parameters

---
+++ MN3M/rescursion #1/problemM.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+#define int long long
+#define double long double
+#define all(x) (x).begin(), (x).end()
+
+void fast_io() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+int n , k , a , b;
+
+int cost(int x) {
+    if(x == 1) return 0;
+    if(x < k) return (x - 1) * a;
+    if(x % k != 0) return (x % k) * a + cost(x - x % k);
+    return min(b , (x - x / k) * a) + cost(x / k);
+}
+
+int32_t main() {
+    fast_io();
+
+    cin >> n >> k >> a >> b;
+
+    cout << cost(n);
+
+    return 0;
+}
